f2eb46dfb0e2998aaa245881e2199537ca581943
Osifo John <osifojohntec@gmail.com>
2023-03-17 03:03:17 +0100

    added rentals endpoint

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const app = express();
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
+const rentals = require('./routes/rentals');
 
 app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
+app.use('/api/rentals', rentals);
 
 mongoose
   .connect('mongodb://127.0.0.1:27017/vidly')
