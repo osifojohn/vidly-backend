3cc2209e041fd2eaa4d56114b52a1d7aa3761c02
Osifo John <osifojohntec@gmail.com>
2023-03-18 15:03:08 +0100

    added users endpoint

--- index.js
+++ index.js
@@ -2,19 +2,20 @@ const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 
-// const Fawn = require('fawn');
-// Fawn.init(mongoose);
-
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
+const users = require('./routes/users');
+const auth = require('./routes/auth');
 
 app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
+app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 mongoose
   .connect('mongodb://127.0.0.1:27017/vidly')
